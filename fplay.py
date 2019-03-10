f9578bcb0bca62dbe4469fb5bd46dea48b20e7ed
AdonisFlint <Adonis7952@hotmail.com>
2019-03-10 21:00:09 +0500

    Добавлнеа задержка 2.5 секунды между нажатиями кнопки

--- fplay.py
+++ fplay.py
@@ -40,7 +40,7 @@ def button_callback(channel):
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback, bouncetime=1000)
+GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback, bouncetime=2500)
 print('GPIO inited')
 
 try:
