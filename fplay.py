aea2e17921ce4c2918ed161b924685baae46f466
AdonisFlint <Adonis7952@hotmail.com>
2019-03-10 15:53:35 +0500

    Исправлен баг из-за которого скрипт не мог обнаружить новую флешку

--- fplay.py
+++ fplay.py
@@ -3,65 +3,46 @@
 import RPi.GPIO as GPIO
 import pygame
 import os, glob, time
+
 a=[]
-mp3_directory=[] # Каталоги всех mp3 на флешке
 
-
-
-def check(): # Проверка флешки
-    check_status = False
-    tree = os.walk('//media/pi')
+def find_sound():
+    tree = os.walk('/media/pi')
     for t in tree:
         a.append(t)
         break
 
-
     if len(a[0][1]) > 0:
-        #print(a[0][1][0])
         flash_name = a[0][1][0]
-        check_status = True
-        for file in os.listdir("//media/pi/" + flash_name):
-            if file.endswith(".mp3"):
-                #print(os.path.join("//media/pi/" + flash_name, file)) #Выводит полную директорию музыкальных файлов
-                mp3_directory.append(os.path.join("//media/pi/" + flash_name, file))
-
+        print("flash drive found: " + flash_name)
+        for file in os.listdir("/media/pi/" + flash_name):
+            if file.endswith(".mp3") or file.endswith(".wav"):
+                sound_path = os.path.join("/media/pi/" + flash_name, file)
+                print("sound found at: " + sound_path)
+                return sound_path
     else:
         print('Error, no usb folder')
         a.clear()
-        mp3_directory.clear()
-        tree = os.walk('//media/pi')
+        tree = os.walk('/media/pi')
         for t in tree:
             a.append(t)
             break
-
-
-
-    return(check_status)
-
+    return None
 
 def button_callback(channel):
     print('button pushed')
-
-    if check():
-
+    sound_path = find_sound()
+    if sound_path is not None:
         pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=4096)
-        pygame.mixer.music.load(mp3_directory[0])
+        pygame.mixer.music.load(sound_path)
         pygame.mixer.music.set_volume(2)
         pygame.mixer.music.play(0)
-'''
-        while pygame.mixer.music.get_busy() == True:
-            continue
-        pygame.quit()
-'''
-
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-
-GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback, bouncetime=1000)
-print('add_event_detect')
+GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback, bouncetime=1000)
+print('GPIO inited')
 
 try:
   while True:
